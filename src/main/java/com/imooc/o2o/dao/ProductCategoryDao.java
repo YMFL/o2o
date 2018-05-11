62912bb8a5454a6111862d0cc840d83d55b22290
yqh <395041293@qq.com>
2018-05-11 15:23:09 +0800

    商品类别批量添加

--- src/main/java/com/imooc/o2o/dao/ProductCategoryDao.java
+++ src/main/java/com/imooc/o2o/dao/ProductCategoryDao.java
@@ -1,6 +1,8 @@
 package com.imooc.o2o.dao;
 
+import com.imooc.o2o.dto.ProductCategoryExecution;
 import com.imooc.o2o.entity.ProductCategory;
+import com.imooc.o2o.exceptions.ProductCategoryOperationException;
 
 import java.util.List;
 
@@ -11,4 +13,19 @@ public interface ProductCategoryDao {
      * @return List<ProductCategory>
      */
     List<ProductCategory> queryProductCategoryList(long shopId);
+
+    /**
+     * 批量新增商品类别
+     * @param productCategoryList
+     * @return
+     */
+    int batchInsertProductCategory(List<ProductCategory> productCategoryList);
+
+    /**
+     *
+     * @param productCategoryList
+     * @return
+     * @throws ProductCategoryOperationException
+     */
+    ProductCategoryExecution batchAddProductCategory(List<ProductCategory> productCategoryList) throws ProductCategoryOperationException;
 }
--- src/test/java/com/imooc/o2o/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ProductCategoryDaoTest.java
@@ -2,9 +2,12 @@ package com.imooc.o2o.dao;
 
 import com.imooc.o2o.BaseTest;
 import com.imooc.o2o.entity.ProductCategory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -14,9 +17,30 @@ public class ProductCategoryDaoTest extends BaseTest {
     private ProductCategoryDao productCategoryDao;
 
     @Test
+    @Ignore
     public void queryProductCategoryList() throws Exception {
         long shopId=6;
         List<ProductCategory> productCategoryList=productCategoryDao.queryProductCategoryList(shopId);
         System.out.println("该店铺自定义类别数为："+productCategoryList.size());
+    }
+
+    @Test
+    public void testBatchInsertProductCategory(){
+        ProductCategory productCategory =new ProductCategory();
+        productCategory.setProductCategoryName("商品类别1");
+        productCategory.setPriority(1);
+        productCategory.setCreateTime(new Date());
+        productCategory.setShopId(1L);
+        ProductCategory productCategory2 =new ProductCategory();
+        productCategory2.setProductCategoryName("商品类别2");
+        productCategory2.setPriority(2);
+        productCategory2.setCreateTime(new Date());
+        productCategory2.setShopId(1L);
+        List<ProductCategory> productCategoryList=new ArrayList<ProductCategory>();
+        productCategoryList.add(productCategory);
+        productCategoryList.add(productCategory2);
+        int effectedNum=productCategoryDao.batchInsertProductCategory(productCategoryList);
+        assertEquals(2,effectedNum);
+
     }
 }
